d1489559113c0ec78bb42dec7e17603ef744e728
selva-shopmatic <selvaganapathi.codingmart@goshopmatic.com>
2024-07-05 20:19:10 +0530

    Return all negative numbers in exception

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -33,5 +33,9 @@ describe StringCalculator do
   it 'raises an exception for negative numbers' do
     expect { @calculator.add('1,-2,3') }.to raise_error('negatives not allowed: -2')
   end
+
+  it 'raises an exception with all negative numbers' do
+    expect { @calculator.add('1,-2,3,-4') }.to raise_error('negatives not allowed: -2, -4')
+  end
   
 end
