2b3ea23e1e585e90809ed862f666bd43f5635b9e
selva-shopmatic <selvaganapathi.codingmart@goshopmatic.com>
2024-07-05 20:09:53 +0530

    Eeturns the sum of two numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,4 +14,8 @@ describe StringCalculator do
     expect(@calculator.add('1')).to eq(1)
   end
 
+  it 'returns the sum of two numbers' do
+    expect(@calculator.add('1,2')).to eq(3)
+  end
+  
 end
