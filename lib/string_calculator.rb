418ba3991af64971e34cbad32db8432ca51e91a9
selva-shopmatic <selvaganapathi.codingmart@goshopmatic.com>
2024-07-05 20:22:34 +0530

    Support delimiters of any length

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -10,10 +10,10 @@ class StringCalculator
     delimiter = /,|\n/
     if numbers.start_with?("//")
       delimiter, numbers = numbers.split("\n", 2)
-      delimiter = delimiter[2..]
+      delimiter = Regexp.escape(delimiter[2..-1].chop)
     end
 
-    num_array = numbers.split(delimiter).map(&:to_i).reject { |num| num > 1000 }
+    num_array = numbers.split(Regexp.new(delimiter)).map(&:to_i).reject { |num| num > 1000 }
     negatives = num_array.select { |num| num < 0 }
     raise "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
 
